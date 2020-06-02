441ebb73ec8afccba658213417ce4677a861405d
Dienay Lima <dienaylima@gmail.com>
2020-06-02 14:16:27 -0300

    Interpretação de código - exercício 2

---
+++ semana3/logica-booleana/interpretacao-de-codigo/exercicio2/index.js
@@ -0,0 +1,25 @@
+let array
+console.log('I. ', array)
+
+array = null
+console.log('II. ', array)
+
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('III. ', array.length)
+
+let i = 0
+console.log('IV. ', array[i], " e ", array[i+1])
+
+array[i+1] = 19
+const valor = array[i+6]
+console.log('V. ', array[i+1], " e ", valor)
+
+i+=1
+array[i] = array[i-1]
+console.log('VI. ', array[i])
+
+i = array.length - 1
+array[i] = array[i-3]
+const resultadoC = array[i]%array[1]
+console.log('VII. ', resultadoC)
