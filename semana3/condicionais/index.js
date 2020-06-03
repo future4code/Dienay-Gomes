a0137849b4346ed4875bb70e358e27c0fdc55d16
Dienay Lima <dienaylima@gmail.com>
2020-06-03 15:20:18 -0300

    Condicionais - Interpretação de código exercício 3

--- semana3/condicionais/index.js
+++ semana3/condicionais/index.js
@@ -39,15 +39,38 @@
 // console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
 
 
-// // a. Para que serve o código acima?
-// //      Para verificar o valor das frutas
+// a. Para que serve o código acima?
+//      Para verificar o valor das frutas
 
-// // b. Qual será a mensagem impressa no console, se o valor de fruta for `"Maçã"`?
-// //      O preço da fruta  Maçã  é  R$  2.25
+// b. Qual será a mensagem impressa no console, se o valor de fruta for `"Maçã"`?
+//      O preço da fruta  Maçã  é  R$  2.25
 
-// // c. Considere que você vá ao mercado com o objetivo de comprar 2 laranjas, 1 maçã, 3 bananas e 1 uva. Qual seria o preço que você pagaria?
-// //      ( 3.5 * 2 ) + 2.25 + ( 5 * 3) + 0.30 = 24.55
+// c. Considere que você vá ao mercado com o objetivo de comprar 2 laranjas, 1 maçã, 3 bananas e 1 uva. Qual seria o preço que você pagaria?
+//      ( 3.5 * 2 ) + 2.25 + ( 5 * 3) + 0.30 = 24.55
 
-// // d. Considere que um usuário queira comprar uma Pêra, qual seria a mensagem impressa no console se retirássemos o break que está logo acima do deafult (o break indicado pelo comentário "BREAK PARA O ITEM d.")?
-// //      O preço da fruta  Pêra  é  R$  5, porque o break é quem faz sair da execução
+// d. Considere que um usuário queira comprar uma Pêra, qual seria a mensagem impressa no console se retirássemos o break que está logo acima do deafult (o break indicado pelo comentário "BREAK PARA O ITEM d.")?
+//      O preço da fruta  Pêra  é  R$  5, porque o break é quem faz sair da execução
  
+
+//EXERCÍCIO 3
+
+// const numero1 = prompt("Digite o primeiro número.")
+// const numero2 = prompt("Digite o próximo número?")
+
+// if(numero1 > 0 && numero2 > 0) {
+//   let mensagem
+//   if(numero1 > numero2) {
+//     mensagem = "Número 1 é maior que o 2!"
+//   } else {
+//     mensagem = "Número 1 é menor ou igual ao 2!"
+//   }
+// }
+
+// console.log(mensagem)
+
+// Considere um usuário que digita os números 3 e 4 respectivamente.Qual será a mensagem do terminal? Haverá algum erro? Justifique usando os conceitos de bloco ou escopo.
+//      Vai dar um erro pois a variável "mensagem" foi declarada no escopo da condição filha.
+//      A variável "mensagem" foi declarada em um escopo local e o console.log chamou ela no escopo global.
+
+
+
