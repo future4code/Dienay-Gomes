4e44d7df60243e49347a0d26960aa1c31a40c654
Dienay Lima <dienaylima@gmail.com>
2020-06-03 15:00:10 -0300

    Condicionais - Interpretação de código exercício 2

--- semana3/condicionais/index.js
+++ semana3/condicionais/index.js
@@ -15,4 +15,39 @@
 //   console . log ( "Não passou no teste." ) }  
 
 
+// //EXERCÍCIO 2
 
+// let fruta = prompt("Escolha uma fruta")
+// let preco
+// switch (fruta) {
+//   case "Laranja":
+//     preco = 3.5
+//     break;
+//   case "Maçã":
+//     preco = 2.25
+//     break;
+//   case "Uva":
+//     preco = 0.30
+//     break;
+//   case "Pêra":
+//     preco = 5.5
+//     break; // BREAK PARA O ITEM d.
+//   default:
+//     preco = 5
+//     break;
+// }
+// console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
+
+
+// // a. Para que serve o código acima?
+// //      Para verificar o valor das frutas
+
+// // b. Qual será a mensagem impressa no console, se o valor de fruta for `"Maçã"`?
+// //      O preço da fruta  Maçã  é  R$  2.25
+
+// // c. Considere que você vá ao mercado com o objetivo de comprar 2 laranjas, 1 maçã, 3 bananas e 1 uva. Qual seria o preço que você pagaria?
+// //      ( 3.5 * 2 ) + 2.25 + ( 5 * 3) + 0.30 = 24.55
+
+// // d. Considere que um usuário queira comprar uma Pêra, qual seria a mensagem impressa no console se retirássemos o break que está logo acima do deafult (o break indicado pelo comentário "BREAK PARA O ITEM d.")?
+// //      O preço da fruta  Pêra  é  R$  5, porque o break é quem faz sair da execução
+ 
