7e7bac110ef3f0b089230ed2ff526c08e4b3d5c9
Dienay Lima <dienaylima@gmail.com>
2020-06-03 14:59:35 -0300

    Condicionais - Interpretação de código exercício 1

---
+++ semana3/condicionais/index.js
@@ -0,0 +1,18 @@
+//Exercícios de interpretação de código
+//EXERCÍCIO 1
+
+// R - Pega um número fornecido pelo usuário
+//     compara o resto da divisão desse número por 2 com 0(zero)
+//     se a comparação estiver correta retorna "Passou no teste" = número par
+//     se não retorna "Não passou no teste" = número impar
+
+// const respostaDoUsuario = prompt ( "Digite o número que você deseja testar?" )
+// const número = número ( respostaDoUsuario ) 
+ 
+
+// if ( numero % 2 === 0 ) { 
+//   console . log ( "Passou no teste." ) } else { 
+//   console . log ( "Não passou no teste." ) }  
+
+
+
