a26892528d9dae9c36394e5a79ac8fdce4034641
Dienay Lima <dienaylima@gmail.com>
2020-06-05 20:41:55 -0300

    Projeto Semana 3 - etapa 9

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -14,8 +14,10 @@ console.log("Bem vindo ao jogo de Blackjack!")
 
 let carta1Usuario = comprarCarta()
 let carta2Usuario = comprarCarta()
+let carta3Usuario = comprarCarta()
 let carta1Cpu = comprarCarta()
 let carta2Cpu = comprarCarta()
+let carta3Cpu = comprarCarta()
 let resultadoUser = 0
 let resultadoCpu = 0
 
@@ -29,11 +31,21 @@ if (confirm("Quer iniciar uma nova rodada?")) {
       console.log(carta2Usuario, carta1Usuario, carta2Cpu, carta1Cpu)
    }
 
-   resultadoUser = (carta1Usuario.valor) + (carta2Usuario.valor)
-   console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto}  - pontuação ${resultadoUser}`)
+   confirm("Deseja comprar mais uma carta?")
 
-   resultadoCpu = (carta1Cpu.valor) + (carta2Cpu.valor)
-   console.log(`Computador - cartas: ${carta1Cpu.texto} ${carta2Cpu.texto}  - pontuação ${resultadoCpu}`)
+
+   if (confirm(`Suas cartas são ${carta1Usuario.texto} ${carta2Usuario.texto}. A carta revelada do computador é ${carta1Cpu.texto}.`+ "\n"+ "Deseja comprar mais uma carta?")) {
+      carta3Usuario = comprarCarta()
+      carta3Cpu = comprarCarta()
+   } else {
+
+   }
+
+   resultadoUser = (carta1Usuario.valor) + (carta2Usuario.valor) + (carta3Usuario.valor)
+   console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} ${carta3Usuario.texto} - pontuação ${resultadoUser}`)
+
+   resultadoCpu = (carta1Cpu.valor) + (carta2Cpu.valor) + (carta3Cpu.valor)
+   console.log(`Computador - cartas: ${carta1Cpu.texto} ${carta2Cpu.texto} ${carta3Cpu.texto} - pontuação ${resultadoCpu}`)
 
    if (resultadoUser > resultadoCpu || resultadoUser === 21) {
       console.log("O usuário ganhou!")
