b691aec9c33d97fdfd1f3ca44ddc70c4bc3bd9be
Dienay Lima <dienaylima@gmail.com>
2020-06-05 12:09:11 -0300

    Projeto Semana 3 - etapa 5

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -12,8 +12,28 @@
  */
 console.log("Bem vindo ao jogo de Blackjack!")
 
+let carta1Usuario = 0
+let carta2Usuario = 0
+let carta1Cpu = 0
+let carta2Cpu = 0
+let resultadoUser = 0
+let resultadoCpu = 0
+
 if(confirm("Quer iniciar uma nova rodada?")) {
    // o que fazer se o usuário clicar "ok"
+   carta1Usuario  = comprarCarta()
+   carta2Usuario  = comprarCarta()
+
+   resultadoUser = (carta1Usuario.valor) + (carta2Usuario.valor)
+   console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto}  - pontuação ${resultadoUser}`)
+   
+
+   carta1Cpu = comprarCarta()
+   carta2Cpu = comprarCarta()
+
+   resultadoCpu = (carta1Cpu.valor) + (carta2Cpu.valor)
+   console.log(`Computador - cartas: ${carta1Cpu.texto} ${carta1Cpu.texto}  - pontuação ${resultadoCpu}`)
+
 } else {
    // o que fazer se o usuário clicar "cancelar"
    console.log("O jogo acabou")
