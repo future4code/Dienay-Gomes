94b799c42acbac1c9572c755a3d2ae318672c9fe
Dienay Lima <dienaylima@gmail.com>
2020-06-08 14:49:14 -0300

    Funções - Interpretação de código, exercício 2

--- semana4/funcoes/index.js
+++ semana4/funcoes/index.js
@@ -21,3 +21,27 @@
 //  [0, 1, 0, 1, 2, 3] no primeiro for quantidade vale 5, mas i vale 0 então não entra no segundo for. Na primeira repetição i vale 2 então vai entrar no segundo for haverá duas repetições e add 0 e 1 no array ficando array[0, 1]. Na proxima repetição do primeiro for i vale 4, então teram 4 repetições no segundo for adicionando 0, 1, 2, 3 no array, ficando array[0, 1, 0, 1, 2, 3]
 // c. Indique qual será o resultado da função caso ela seja chamada como minhaFuncao(8)
 //    seguindo o raciocínio das questões anteriores o i do for mais externos vai ter consequente os valores 0, 2, 4 e 6. o for de dentro terá 3 cíclos gerando [0, 1], [0, 1, 2, 3] e [0, 1, 2, 3, 4, 5] e como push sempre adiciona os valores no final do array vai ficar array[0, 1, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5]
+
+
+//EXERCÍCIO 2
+
+// let arrayDeNomes = ["Darvas", "Goli", "João", "Paulinha", "Soter"];
+
+// const funcao = (lista, nome) => {
+//   for (let i = 0; i < lista.length; i++) {
+//     if (lista[i] === nome) {
+//       return i;
+//     }
+//   }
+// };
+
+// console.log(funcao(arrayDeNomes, "Darvas"));
+// console.log(funcao(arrayDeNomes, "João"));
+// console.log(funcao(arrayDeNomes, "Paula"));
+
+// a. Explicite quais são as saídas impressas no console
+//  Essa função pede um array e um nome como argumentos. Ela olha dentro do array e "vê" se o nome fornecido e retorna o indice onde esse nome se encontra no array, sendo assim o resultados de cada console.log será 0 e 2, como o nome "Paula" não está no array não vai retornar para essa opção
+
+// b. O código funcionaria se a lista fosse um array de números (ao invés de um array de string)  e o nome fosse um número, ao se chamar a função? Justifique sua resposta.
+//  funcionaria, pois os valores contidos num array são acessados da mesma forma independente do tipo.
+
