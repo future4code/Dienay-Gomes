58911ccfcbcbdcb1c4601cd892c4da2f080d8812
Dienay Lima <dienaylima@gmail.com>
2020-06-12 11:54:18 -0300

    Lista de Exercícios - Interpretação de código - 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,7 +1,20 @@
 //Exercícios de interpretação de código
 
-//1
+//EXERCÍCIO 1
+// Essa função converte o quanto você tem em dólares em reais dependendo da cotação que você fornecer no prompt
+
 // a função conversorDeMoeda solicita uma entrada de um valor em dólar que no caso será 100 dólares como mostra na chamada da ultima linha escrita de código.
 // dentro dessa função é solicitado uma entrada do usuário através do prompt do valor da cotação do dolar, esse valor é convertido em number logo na entrada.
 // ela vai retornar esse valor em reais multiplicando o valor adicionado na chamada da função pelo introduzido pelo usuário que é o da cotação.
+
 // Hoje o console.log vai retornar R$501,97
+
+
+//EXERCÍCIO 2
+// Essa função recebe um tipo de investimento e um valor para esse investimento, compara ele, e devolve o valor atualizado baseado no tipo de investimento.
+
+// Primeiro o switch vai pegar o valor inserido no primeiro argumento da função, compara a cada caso específico criados nele, caso não tenha nada que se encaixe a esses casos ele entra no default e da a mensagem de tipode investimento incorreto. Mas caso ache o seu correspondente ele entra do trecho do case, pega o valor do segundo argumento da função e multiplica pelo valor que está definido nesse case específico, e põe esse valor numa variável que vai servir de retorno da função.
+
+// console.log de novoMontante terá o resultado de 165
+// edo segundoMontante será "TIPO DE INVESTIMENTO INFORMADO INCORRETO"
+
