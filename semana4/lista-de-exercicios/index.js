37c0cf746b662a2afddb479b5264df0b25bfdef5
Dienay Lima <dienaylima@gmail.com>
2020-06-12 11:11:46 -0300

    Lista de Exercícios - Interpretação de código - 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,7 @@
+//Exercícios de interpretação de código
+
+//1
+// a função conversorDeMoeda solicita uma entrada de um valor em dólar que no caso será 100 dólares como mostra na chamada da ultima linha escrita de código.
+// dentro dessa função é solicitado uma entrada do usuário através do prompt do valor da cotação do dolar, esse valor é convertido em number logo na entrada.
+// ela vai retornar esse valor em reais multiplicando o valor adicionado na chamada da função pelo introduzido pelo usuário que é o da cotação.
+// Hoje o console.log vai retornar R$501,97
