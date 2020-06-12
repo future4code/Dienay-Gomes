d3c9faceb14b8a43a6930c0547015d42e0fa7720
Dienay Lima <dienaylima@gmail.com>
2020-06-11 21:32:14 -0300

    Callback e funções de Array - filtros quase protos, mas não tô conseguindo fazer aparecer no html todos os itens quando clico em filtro

--- semana4/funcoes-de-array/controlador-de-despesas/index.js
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -1,44 +1,84 @@
-const totaDeDespesas = []
+const totalDeDespesas = []
 
 function cadastraDespesa() {
     const valor = document.getElementById("valor")
-    const tipo  = document.getElementById("tipo")
+    const tipo = document.getElementById("tipo")
     const descricao = document.getElementById("descricao")
-    console.log(valor.value)
-    console.log(tipo.value)
-    console.log(descricao.value)
 
     if ((valor.value === "") || (tipo.value === "") || (descricao === "")) {
         alert("Não é possível criar despesa vazia")
-        console.log(totaDeDespesas)
+        console.log(totalDeDespesas)
     } else {
 
-        
-        const despesa = {
-            valor : valor.value ,
-            tipo : tipo.value ,
-            descricao : descricao.value
-        }
-        
-        totaDeDespesas.push(despesa)
-        
-        console.log(despesa)
-        console.log(totaDeDespesas)
-        limpaFiltros() 
-    }
 
+        const despesa = {
+            valor: valor.value,
+            tipo: tipo.value,
+            descricao: descricao.value
+        }
+
+        totalDeDespesas.push(despesa)
+        let imprimir = document.getElementById("imprimir")
+        imprimir.innerHTML += `<p> R$ ${despesa.valor} na categoria ${despesa.tipo} para ${despesa.descricao}</p>`
+
+        console.log(despesa)
+        console.log(totalDeDespesas)
+        // valor.value = ""
+        // tipo.value = ""
+        // descricao.value = ""
+        
+    }
 }
 
-console.log(totaDeDespesas)
 
-function filtra(){
-    const valor = document.getElementById("valor")
-    const tipo  = document.getElementById("tipo")
-    const descricao = document.getElementById("descricao")
+function filtra() {
+    const tipoDetalhe = document.getElementById("tipoDetalhe")
+    const minimo = document.getElementById("minimo")
+    const maximo = document.getElementById("maximo")
+
+    if (tipoDetalhe.value === "casa") {
+        const filtrarGastos = totalDeDespesas.filter((item, i, a) => {
+            if (item.tipo === "casa") { // = return pokemon.tipo === "grama"
+                return true
+            }
+            return false
+        })
+            console.log(filtrarGastos)
+    }
+
+    if (tipoDetalhe.value === "viagem") {
+        const filtrarGastos = totalDeDespesas.filter((item, i, a) => {
+            if (item.tipo === "viagem") { // = return pokemon.tipo === "grama"
+                return true
+            }
+            return false
+        })
+        console.log(filtrarGastos)
+    }
+
+    if (tipoDetalhe.value === "rango") {
+        const filtrarGastos = totalDeDespesas.filter((item, i, a) => {
+            if (item.tipo === "rango") { // = return pokemon.tipo === "grama"
+                return true
+            }
+            return false
+        })
+        console.log(filtrarGastos)
+    }
+
+    if (tipoDetalhe.value === "outros") {
+        const filtrarGastos = totalDeDespesas.filter((item, i, a) => {
+            if (item.tipo === "outros") { // = return pokemon.tipo === "grama"
+                return true
+            }
+            return false
+        })
+        console.log(filtrarGastos)
+    }
 }
 
 function limpaFiltros() {
-    valor.value = ""
-    tipo.value = ""
-    descricao.value = ""
+    tipoDetalhe.value = ""
+    minimo.value = ""
+    maximo.value = ""
 }
