8ac3dc9f9e7b13678bb07c73569463353a39981f
Dienay Lima <dienaylima@gmail.com>
2020-06-14 16:50:50 -0300

    Lista de Exercícios - Funções de Array - 5

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -357,3 +357,20 @@
 
 // console.log(emails)
 
+// EXERCÍCIO 5
+// const contas = [
+// 	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+// 	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+// 	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+// 	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+// 	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+// 	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+// ]
+
+// contas.forEach((e, i, a) => {
+//     e.saldoTotal = e.saldoTotal - (e.compras.reduce(function(t, e) {
+//         return t + e
+//     }, 0))
+// })
+
+// console.log(contas)
