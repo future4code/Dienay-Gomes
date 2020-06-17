29ad3b30bc7938d4e87cedf00b022a1660c223fe
Dienay Lima <dienaylima@gmail.com>
2020-06-17 14:45:50 -0300

    Componentes de classe - 2 Posts criados

--- semana5/insta4 2/src/App.js
+++ semana5/insta4 2/src/App.js
@@ -11,6 +11,16 @@ class App extends React.Component {
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150'}
         />
+        <Post
+          nomeUsuario={'joãozinho'}
+          fotoUsuario={'https://picsum.photos/400/400?a=1'}
+          fotoPost={'https://picsum.photos/400/400?a=2'}
+        />
+        <Post
+          nomeUsuario={'jonisverson'}
+          fotoUsuario={'https://picsum.photos/400/400?a=3'}
+          fotoPost={'https://picsum.photos/400/400?a=4'}
+        />
       </div>
     );
   }
