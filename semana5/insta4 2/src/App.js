740108e35509132a94effb6f523197075f244928
Dienay Lima <dienaylima@gmail.com>
2020-06-18 16:51:20 -0300

    Listas e Estilização

--- semana5/insta4 2/src/App.js
+++ semana5/insta4 2/src/App.js
@@ -3,24 +3,87 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+
+  state = {
+    posts: [
+      {
+        nome: "paulinha",
+        fotoUser: "https://picsum.photos/50/50?a=5",
+        fotoPost: "https://picsum.photos/300/300?a=6"
+      },
+      {
+        nome: "joãozinho",
+        fotoUser: "https://picsum.photos/50/50?a=1",
+        fotoPost: "https://picsum.photos/300/300?a=2"
+      },{
+        nome: "jonisverson",
+        fotoUser: "https://picsum.photos/50/50?a=3",
+        fotoPost: "https://picsum.photos/300/300?a=4"
+      }
+    ],
+
+    valorInputUser: "",
+    valorInputFotoUser: "",
+    valorInputFotoPost: "" 
+  }
+
+  postar = () => {
+    const novoPost = {
+      nome: this.state.valorInputUser,
+      fotoUser: this.state.valorInputFotoUser,
+      fotoPost: this.state.valorInputFotoPost
+    }
+
+    const postando = [...this.state.posts, novoPost]
+
+    this.setState({posts: postando})
+    this.setState({valorInputUser: "", valorInputFotoUser: "", valorInputFotoPost: ""})
+  }
+
+  onChangeUser = event => {
+    this.setState({valorInputUser: event.target.value})
+  }
+
+  onChangeFotoUser = event => {
+    this.setState({valorInputFotoUser: event.target.value})
+  }
+
+  onChangeFotoPost = event => {
+    this.setState({valorInputFotoPost: event.target.value})
+  }
+
   render() {
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'joãozinho'}
-          fotoUsuario={'https://picsum.photos/400/400?a=1'}
-          fotoPost={'https://picsum.photos/400/400?a=2'}
-        />
-        <Post
-          nomeUsuario={'jonisverson'}
-          fotoUsuario={'https://picsum.photos/400/400?a=3'}
-          fotoPost={'https://picsum.photos/400/400?a=4'}
-        />
+        <div>
+          <input
+            value={this.state.valorInputUser}
+            onChange={this.onChangeUser}
+            placeholder={'Nome de usuário'}
+          />
+          <input
+            value={this.state.valorInputFotoUser}
+            onChange={this.onChangeFotoUser}
+            placeholder={'Foto do usuário'}
+          />
+          <input
+            value={this.state.valorInputFotoPost}
+            onChange={this.onChangeFotoPost}
+            placeholder={'Foto do post'}
+          />
+          <button onClick={this.postar}>Postar</button>
+        </div>
+        {this.state.posts.map((post) => {
+      // Porém, cada pessoa não é mais uma string somente, mas sim um objeto
+      // com duas propriedades. Por isso, no momento de colocá-la na tela
+      // precisamos explicitar qual das propriedades queremos mostrar em
+      // cada lugar.
+      return <Post
+              nomeUsuario={post.nome}
+              fotoUsuario={post.fotoUser}
+              fotoPost={post.fotoPost}
+            />
+        })}
       </div>
     );
   }
