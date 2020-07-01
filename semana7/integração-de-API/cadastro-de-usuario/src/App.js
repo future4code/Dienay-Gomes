dea8eba112753014b32ff452a020dc2e356f1ed4
Dienay Lima <dienaylima@gmail.com>
2020-07-01 17:21:54 -0300

    Exercício de Integrações de API - componetização, alguns ajustes e primeiro desafio completo

--- semana7/integração-de-API/cadastro-de-usuario/src/App.js
+++ semana7/integração-de-API/cadastro-de-usuario/src/App.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import './App.css';
-import axios from 'axios';
 import styled from 'styled-components'
 import ListUsers from './components/ListUsers';
-import icon from './icons/trash.svg'
+import CreateUser from './components/CreateUser';
 
 const ButtonChange = styled.button`
   position:fixed;
   top:0;
   left: 0;
-`
-
-const EachUser = styled.div`
-  margin: 60px auto;
-  width: 300px;
-
-  > div {
-    align-items: center;
-    border-bottom: 1px solid #283e4a;
-    display: flex;
-    justify-content: space-between;
-  }
-
-  img {
-    width: 20px;
-  }
-
-  img:hover {
-    color: red;
-  }
 `
 
 const ContainerForm = styled.div`
@@ -73,137 +52,31 @@ const Form = styled.div`
 
 class App extends React.Component {
   state = {
-    userList: [],
-    userValue: "",
-    emailValue: "",
-    toExchangeComponent: false,
+    toExchangeComponent: true,
   }
-
-  componentDidMount = () => {
-    this.getUser()
-  }
-  
-  getUser = () => {
-    axios
-    .get(
-      'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users',
-      {
-        headers: {
-          Authorization: 'dienay-lima-turing'
-        }
-      }
-      ).then(response => {
-        this.setState({userList: response.data})
-      }).catch(error => {
-        console.log(error.data)
-      })
-  }
-
-  createUser = () => {
-    const body = {
-      name: this.state.userValue,
-      email: this.state.emailValue
-    }
-
-    axios
-    .post(
-      'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users',
-      body,
-      {
-        headers: {
-          Authorization: 'dienay-lima-turing'
-        }
-      }).then(response => {
-        this.getUser()
-        this.setState({userValue: ""})
-        this.setState({emailValue: ""})
-        alert("O usuario foi adicionado com sucesso!")
-      }
-    ).catch(error => {
-      console.log(error.data)
-      alert("Não foi possível adicionar o usuário!")
-    })
-  }
-
-  deleteUser = (userId) => {
-    axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${userId}`,
-    {
-      headers: {
-        Authorization: "dienay-lima-turing"
-        }
-    }
-      )
-      .then(response => {
-        this.getUser()
-        alert("O usuario foi deletado com sucesso!")
-      })
-      .catch(error => {
-        alert("Não foi possível deletar o usuário!")
-      })
-    }
 
   changeComponent = () => {
     this.setState({ toExchangeComponent: !this.state.toExchangeComponent })
   }
 
-  onChangeUserValue = event => {
-    this.setState({userValue: event.target.value})
-  }
-
-  onChangeEmailValue = event => {
-    this.setState({emailValue: event.target.value})
-  }
-
-  componentDidUpdate = () => {
-    console.log(this.toExchangeComponent)
-  }
 
   render() {
+    const buttonName =
+      this.state.toExchangeComponent === false
+        ? "Ir para lista"
+        : "Ir para cadastro";
     return (
       <div className="App">
         <div>
           <ButtonChange 
-          onClick={this.changeComponent}>Mudar a página</ButtonChange>
+          onClick={this.changeComponent}>{buttonName}</ButtonChange>
         </div>
+
         <div>
-          {
-          this.state.toExchangeComponent ? 
-          <EachUser>
-            <h2>Usuários Cadastrados:</h2>
-            {this.state.userList.map(user => {
-            return <div>
-              <ListUsers
-                key={user.id}
-                mostraUser={user.name}
-              />
-              <button onClick={() => this.deleteUser(user.id)}>X</button>
-            </div>
-          })}
-          </EachUser>
+          {this.state.toExchangeComponent ?
+          <ListUsers />
           :
-          <ContainerForm>
-            <Form>
-                <div className="field">
-                  <label>Nome</label>
-                  <input
-                    value={this.state.userValue}
-                    onChange={this.onChangeUserValue}
-                    placeholder={"Nome de Usuário"}
-                  />
-                </div>
-                <div className="field">
-                  <label>E-mail</label>
-                  <input
-                    value={this.state.emailValue}
-                    onChange={this.onChangeEmailValue}
-                    placeholder={"E-mail do usuário"}
-                  />
-                </div>
-                <div className="field">
-                  <button onClick={this.createUser}>Criar usuário</button>
-                </div>
-            </Form>
-          </ContainerForm>
+          <CreateUser />
           }
         </div>
       </div>
--- semana7/integração-de-API/cadastro-de-usuario/src/components/ListUsers.js
+++ semana7/integração-de-API/cadastro-de-usuario/src/components/ListUsers.js
@@ -1,10 +1,132 @@
 import React from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+import icon from '../icons/trash.svg'
+
+const baseUrl =
+  "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
+
+const Carregando = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .dots {
+  width: 3.5em;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.dots div {
+  width: 0.8em;
+  height: 0.8em;
+  border-radius: 50%;
+  background-color: rgba(33, 159, 213, 1);
+  animation: fade 0.8s ease-in-out alternate infinite;
+}
+
+.dots div:nth-of-type(1) {
+  animation-delay: -0.4s;
+}
+
+.dots div:nth-of-type(2) {
+  animation-delay: -0.2s;
+}
+
+@keyframes fade {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+`
+
+const List = styled.ul`
+  margin: 60px auto;
+  width: 300px;
+
+  > li {
+    align-items: center;
+    border-bottom: 1px solid #283e4a;
+    display: flex;
+    justify-content: space-between;
+    padding: 4px 0;
+  }
+
+  img {
+    width: 18px;
+  }
+`
 
 class ListUsers extends React.Component {
+    state = {
+        userList: []
+      }
+
+    componentDidMount = () => {
+        this.getUser()
+    }
+      
+    getUser = () => {
+        axios
+        .get(
+          baseUrl,
+          {
+            headers: {
+              Authorization: 'dienay-lima-turing'
+            }
+          }
+          ).then(response => {
+            this.setState({userList: response.data})
+          }).catch(error => {
+            console.log(error.data)
+          })
+    }
+
+    deleteUser = (userId) => {
+        if (window.confirm("Tem certeza que quer apagar esse usuário?")) {
+          axios.delete(`${baseUrl}/${userId}`,
+        {
+          headers: {
+            Authorization: "dienay-lima-turing"
+            }
+        }
+          )
+          .then(response => {
+            this.getUser()
+            alert("O usuario foi deletado com sucesso!")
+          })
+          .catch(error => {
+            alert("Não foi possível deletar o usuário!")
+          })
+        }
+    }
+
     render() {
         return (
             <div>
-                <p>{this.props.mostraUser}</p>
+                <h2>Usuários Cadastrados:</h2>
+                <List>
+                    {this.state.userList.length === 0 && <Carregando>
+                      <div class="dots">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                      </div>
+                    </Carregando>}
+                    {this.state.userList.map(user => {
+                        return (
+                            <li key={user.id}>
+                                {user.name}
+                            <img src={icon} onClick={() => this.deleteUser(user.id)}/>
+                            </li>
+                        )
+                    })}
+                </List>
             </div>
         )
     }
