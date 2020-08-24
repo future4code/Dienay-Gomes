1e134264df4464c2aada8641fe695d746d76bfe8
Dienay Lima <dienaylima@gmail.com>
2020-08-24 20:32:32 -0300

    Introduçao a Typescript - Exercicios.

--- semana14/introducao-typescript/src/exercicio2.ts
+++ semana14/introducao-typescript/src/exercicio2.ts
@@ -1,10 +1,10 @@
 // Exercício 2
 // a. Quais são as entradas e saídas dessa função? Copie a função para um arquivo .ts e faça a tipagem desses parâmetros
 
-function obterEstatisticas(numeros:number[]):number[] {
+function obterEstatisticas(numeros:number[]) {
 
-    const numerosOrdenados = numeros.sort(
-        (a, b) => a - b
+    const numerosOrdenados:number[] = numeros.sort(
+        (a:number, b:number) => a - b
     )
 
     let soma:number = 0
@@ -21,3 +21,20 @@ function obterEstatisticas(numeros:number[]):number[] {
 
     return estatisticas
 }
+console.log(obterEstatisticas([1, 2, 3, 4, 5]))
+
+// b. Que outras variáveis compõem essa função? Explicite a tipagem de todas elas
+// numerosOrdenados:number[] Array
+// let soma:number = 0 Number
+
+// c. Crie um type para representar uma amostra de dados, isto é, um objeto com as chaves numeros e obterEstatisticas. Abaixo, temos um exemplo de objeto desse tipo, declarado em JavaScript:
+const amostraDeIdades = {
+
+    numeros: [21, 18, 65, 44, 15, 18],
+
+    obterEstatisticas: (numeros:number[]) => {
+        return obterEstatisticas
+    }
+}
+
+console.log(amostraDeIdades)
--- semana14/introducao-typescript/src/exercicio3.ts
+++ semana14/introducao-typescript/src/exercicio3.ts
@@ -1,1 +1,43 @@
 //Exercício 3
+//a) Copie o código acima para um arquivo .ts. Depois, crie um type para representar um post e utilize-o para fazer a tipagem do array posts.
+type post = { 
+  autor: string, 
+  texto: string
+}
+
+const posts: post[] = [
+    {
+      autor: "Alvo Dumbledore",
+      texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+      autor: "Severo Snape",
+      texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+      autor: "Hermione Granger",
+      texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+      autor: "Dobby",
+      texto: "Dobby é um elfo livre!"
+    },
+    {
+      autor: "Lord Voldemort",
+      texto: "Avada Kedavra!"
+    }
+  ]
+//   b) Observe abaixo a função buscarPostsPorAutor(), escrita em JavasScript:
+//   Quais são as entradas e saídas dessa função? Copie a função para o mesmo arquivo .ts do array de posts e faça a tipagem necessária. 
+//recebe um array e o nome du autor(opcional) e retorna um array com todos os autores e suas citações ou somente do autor escolhido
+
+  function buscarPostsPorAutor(posts:post[], autorInformado?:string):post[] {
+    return posts.filter(
+      (post) => {
+        return post.autor === autorInformado
+      }
+    )
+  }
+
+  console.log(buscarPostsPorAutor(posts, "Alvo Dumbledore"))
+
