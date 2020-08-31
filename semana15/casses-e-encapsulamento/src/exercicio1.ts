81d023477b818091bf9d3c984431ee15f1068c3b
Dienay Lima <dienaylima@gmail.com>
2020-08-31 16:18:17 -0300

    Classes e Encapsulamento

---
+++ semana15/casses-e-encapsulamento/src/exercicio1.ts
@@ -0,0 +1,41 @@
+// Exercício 1
+// a) Para que serve o construtor dentro de uma classe e como fazemos para chamá-lo?
+//Serve pra inicializar a classe com um valor e esses dados são colocados como atributo na chamada da classe.
+
+// b) Copie o código abaixo para o seu exercício de hoje; crie uma instância dessa classe (dê o nome, cpf e idade que você quiser). Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal?
+//a mensagem apareceu somente uma vez.
+
+// c) Como conseguimos ter acesso às propriedades privadas de uma classe?
+// usando getters e setters
+
+
+// type Transaction = {
+   
+// }
+
+// class UserAccount {
+//     private cpf: string;
+//     private name: string;
+//     private age: number;
+//     private balance: number = 0;
+//     private transactions: Transaction[] = [];
+  
+//     constructor(
+//        cpf: string,
+//        name: string,
+//        age: number,
+//     ) {
+//        console.log("Chamando o construtor da classe UserAccount")
+//        this.cpf = cpf;
+//        this.name = name;
+//        this.age = age;
+//     }
+//   }
+
+// const User: UserAccount = new UserAccount(
+//    "000.000.000-00",
+//    "Dienay",
+//    29
+// )
+
+// console.log(User)
---
+++ semana15/casses-e-encapsulamento/src/exercicio2.ts
@@ -0,0 +1,40 @@
+class Transaction {
+    private date: string;
+    private value: number;
+    private description: string;
+    
+    constructor(date: string, value: number, description: string) {
+      this.date = date;
+      this.value = value;
+      this.description = description
+    }
+  }
+
+  
+
+class UserAccount {
+    private cpf: string;
+    private name: string;
+    private age: number;
+    private balance: number = 0;
+    private transactions: Transaction[] = [];
+  
+    constructor(
+       cpf: string,
+       name: string,
+       age: number,
+    ) {
+       console.log("Chamando o construtor da classe UserAccount")
+       this.cpf = cpf;
+       this.name = name;
+       this.age = age;
+    }
+  }
+
+const User: UserAccount = new UserAccount(
+   "000.000.000-00",
+   "Dienay",
+   29
+)
+
+console.log(User)
